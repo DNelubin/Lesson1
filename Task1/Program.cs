cb41da96289ca84318556748437d690683e500ca
DN <NelubinDO@mail.ru>
2024-03-21 22:06:00 +0300

    задача 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Привет мир");
+﻿class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 5, 10, 20, 30, 40, 99, 4, 90, 3 };
+        int count = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number >= 10 && number <= 90)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10, 90]: {count}");
+    }
+}
