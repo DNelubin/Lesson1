293de831e0a0bf878a0498e1bda1a7d3225bace8
DN <NelubinDO@mail.ru>
2024-03-22 21:31:56 +0300

    Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,28 +1,17 @@
-﻿using System;
-
-public class Answer {
-    static void PrintEvenNumbers(int number)
+﻿public class Program
+{
+    public static void Main()
     {
-      // Введите свое решение ниже
-for (int i = 2; i <= number; i += 2)
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 7 == 0 && number % 23 == 0)
         {
-            Console.Write(i + " ");
+            Console.WriteLine($"Число {number} кратно одновременно 7 и 23");
         }
-    }
-
-
-  // Не удаляйте и не меняйте метод Main! 
-    static public void Main(string[] args) {
-        int number;
-
-        if (args.Length >= 1) {
-            number = int.Parse(args[0]);
-        } else {
-           // Здесь вы можете поменять значения для отправки кода на Выполнение
-            number = 5;
+        else
+        {
+            Console.WriteLine($"Число {number} не кратно одновременно 7 и 23");
         }
-
-        // Не удаляйте строки ниже
-        PrintEvenNumbers(number);
     }
 }
